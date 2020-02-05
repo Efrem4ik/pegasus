5dae35687e5ba2926e96febe65f2149852466e93
Efrem4ik <efrem4ik12@mail.ru>
2020-02-05 17:40:46 +0300

    added animated

--- app/js/main.js
+++ app/js/main.js
@@ -12,30 +12,7 @@ $(function () {
     slidesToScroll: 1,
     variableWidth: true
   });
-  //  $('.slider-for').slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   arrows: false,
-  //   fade: true,
-  //   asNavFor: '.slider-nav'
-  // });
-  // $('.slider-nav').slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   asNavFor: '.slider-for',
-  //   dots: true,
-  //   centerMode: true,
-  //   focusOnSelect: true
-  // }); Двойной слайдер!
 
-
-  // $('.wrapper .tab').on('click', function(event) {
-  // var id = $(this).attr('data-id');
-  //   $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
-  //   $('.wrapper .tabs').find('.tab').removeClass('active');
-  //   $(this).addClass('active');
-  //   $('#'+id).addClass('active-tab').fadeIn();
-  //   return false;
-  // }); Для табов!
+  new WOW().init();
 
 });
